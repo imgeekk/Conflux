03b98481ba964920e05a72b8d881614287c377a4
imgeekk <imgeekkk@gmail.com>
2026-07-09 21:58:46 +0530

    add loader and spinner

---
+++ src/components/Loader.tsx
@@ -0,0 +1,11 @@
+const Loader = () => {
+  return (
+    <div className="loader">
+      <div className="box1" />
+      <div className="box2" />
+      <div className="box3" />
+    </div>
+  );
+};
+
+export default Loader;
