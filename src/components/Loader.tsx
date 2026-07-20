248beaee909c7f0796b1ab6cbb687ac62816fccf
imgeekk <imgeekkk@gmail.com>
2026-07-20 11:58:26 +0530

    add size prop to loader comp

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,11 +1,11 @@
-const Loader = () => {
+const Loader = ({ size = "md" }: { size?: "sm" | "md" | "lg" | "xl" }) => {
+  const scales = { sm: 0.3, md: 0.5, lg: 0.7, xl: 1 }
   return (
-    <div className="loader">
+    <div className="loader" style={{ transform: `scale(${scales[size]})` }}>
       <div className="box1" />
       <div className="box2" />
       <div className="box3" />
     </div>
   );
 };
-
 export default Loader;
