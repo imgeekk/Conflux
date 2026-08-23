cc538d7cc95ab20e59cc5544d6182d7c4829dfd2
imgeekk <imgeekkk@gmail.com>
2026-08-23 22:07:11 +0530

    setting page lint fix

--- src/app/(app)/workspace/settings/page.tsx
+++ src/app/(app)/workspace/settings/page.tsx
@@ -156,7 +156,6 @@ export default function SettingsPage() {
                         </div>
                     )
                     )}
-
                 </CardContent>
             </Card>
         </div>
